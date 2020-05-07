2656b2022ad33af8f1073f4e469485db83b2e922
maxtuchin <64993369+maxtuchin@users.noreply.github.com>
2020-05-08 02:18:31 +0300

    Add files via upload

---
+++ FTPYacc.py
@@ -0,0 +1,31 @@
+from ply import yacc
+from FTPLex import *
+
+
+def p_url(t):
+    "url : SCHEMA DOMAIN secondaryDomains TLD"
+
+    t[0] = t[2][:-1].lower()
+    return t
+
+def p_secondaryDomains(t):
+    """secondaryDomains : DOMAIN secondaryDomains
+                        | empty"""
+    if t[1] == '':
+        t[0] = str()
+        return t
+
+    t[0] = t[1] + t[2]
+    return t
+
+def p_empty(t):
+    "empty : "
+
+    t[0] = str()
+    return t
+
+def p_error(t):
+    pass
+
+
+parser = yacc.yacc(optimize=1)
